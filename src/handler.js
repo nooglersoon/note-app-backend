f06eaf82c2813082ac687d1203b3b87cf09e54fc
nooglersoon <nooglersoon@outlook.co.id>
2024-04-09 17:50:49 +0700

    handle post notes

---
+++ src/handler.js
@@ -0,0 +1,42 @@
+const notes = require('./notes')
+const { nanoid } = require('nanoid');
+
+const addNoteHandler = (request, h) => {
+  const { title, tags, body } = request.payload;
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = {
+    title, tags, body, id, createdAt, updatedAt,
+  };
+
+  // To add the new note to notes stack
+  notes.push(newNote);
+
+  // To check wheter the newNotes has been created
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (isSuccess) {
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil ditambahkan',
+      data: {
+        noteId: id,
+      },
+    });
+    response.code(201);
+    response.header('Access-Control-Allow-Origin', '*');
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal ditambahkan',
+  });
+  response.code(500);
+  return response;
+
+};
+
+module.exports = { addNoteHandler };
--- src/server.js
+++ src/server.js
@@ -1,10 +1,20 @@
 const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
     host: 'localhost',
+    routes: {
+      // Setup cors
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
+
+  // Setup routes
+  server.route(routes);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
